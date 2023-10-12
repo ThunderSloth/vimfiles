13364d59c0a17ee97d62d08730b4cbf0b17ed649
Eli Bell <elibell@Elis-MacBook-Pro.local>
2023-10-12 11:11:47 -0400

    python skeleton

--- templates/skeleton.py
+++ templates/skeleton.py
@@ -2,8 +2,8 @@
 # Eli Bell
 # [:VIM_EVAL:]strftime('%F')[:END_EVAL:]
 
-print("\nWelcome to my program!\n")
+print("\nWelcome to my script!\n")
         
 # ...
 
-print("\n\nThanks for using my program!")
+print("\nThanks for using my script!")
