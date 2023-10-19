ed48baf03493610add86401e547d58abf72cd52d
Eli Bell <elibell@elibell.net>
2023-10-19 10:35:29 -0400

    main to py

--- templates/skeleton.py
+++ templates/skeleton.py
@@ -2,8 +2,13 @@
 # Eli Bell
 # [:VIM_EVAL:]strftime('%F')[:END_EVAL:]
 
-print("\nWelcome to my script!\n")
-        
-# ...
+def main():
+    print("\nWelcome to my script!\n")
 
-print("\nThanks for using my script!")
+    # ...
+
+    print("\nThanks for using my script!")
+
+
+if __name__ == '__main__':
+    main()
